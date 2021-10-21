3fdd233676f9f3e3e35b0b652207a1f714139932
julianalopesp <juli-analopespereira@hotmail.com>
2021-10-21 14:59:08 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const html = document.querySelector('html');
+const checkbox = document.querySelector('input[name=theme]');
+
+const getStyle = (element, style) => 
+    window.getComputedStyle(element).getPropertyValue(style);
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text"),
+}
+
+const darkMode = {
+    bg: "#333333",
+    bgPanel: "#434343",
+    colorHeadings: "#E0618f",
+    colorText: "#B5B5B5"
+}
+
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase();
+
+
+function changeColors(colors) {
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key), colors[key])
+    )
+}
+
+function handleTarget({target}) {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+}
+
+checkbox.addEventListener('change', handleTarget);
+
